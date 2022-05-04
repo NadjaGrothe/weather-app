d72f4c6886ab107b1e205908157651f6095ee08d
Nadja <92398598+NadjaGrothe@users.noreply.github.com>
2022-05-04 08:31:24 +0100

    basic layout created, no styling

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,51 @@
-import React from 'react';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
 
 function App() {
-  return (
-    <div className="App">
-      <h1>Weather App</h1>
-    </div>
-  );
+   const [locationSearch, setLocationSearch] = useState("");
+   const [locations, setLocations] = useState<string[]>([]);
+
+   const disableSearch = locationSearch.trim() === "";
+   const addLocation = () => {
+      setLocations([locationSearch, ...locations]);
+   };
+
+   return (
+      <div className="App">
+         <h1>Weather App</h1>
+         <div>
+            <label>
+               Add Location{" "}
+               <input
+                  type="text"
+                  value={locationSearch}
+                  onChange={(e) => setLocationSearch(e.target.value)}
+               />
+            </label>
+            <button onClick={addLocation} disabled={disableSearch}>
+               Search
+            </button>
+         </div>
+
+         <div>
+            <h2>Locations</h2>
+            <table>
+               <thead>
+                  <tr>
+                     <th>Name</th>
+                  </tr>
+               </thead>
+               <tbody>
+                  {locations.map((location, index) => (
+                     <tr key={index}>
+                        <td>{location}</td>
+                     </tr>
+                  ))}
+               </tbody>
+            </table>
+         </div>
+      </div>
+   );
 }
 
 export default App;
