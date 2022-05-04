f83a685dbf352fd08780c69191113f29188adb87
Nadja <92398598+NadjaGrothe@users.noreply.github.com>
2022-05-04 20:52:18 +0100

    added forecast service

--- src/services/WeatherService.ts
+++ src/services/WeatherService.ts
@@ -37,3 +37,12 @@ export async function readWeather(locationId: number): Promise<Weather> {
 export function getIconUrl(code: string): string {
    return `http://openweathermap.org/img/wn/${code}.png`;
 }
+
+export async function readForecast(locationId: number) :Promise<Weather[]>{
+
+   const forecast = await fetch (`${server}/forecast?id=${locationId}&${keyQuery}&units=metric&cnt=8`)
+
+   if (forecast.status !== 200) throw new Error ("Failed to read location data")
+
+   return (await forecast.json()).list
+}
